723db2570ef87b89d092edd2b1d4605d89622096
SandraHotsyn <85835454+SandraHotsyn@users.noreply.github.com>
2024-12-05 21:12:59 +0200

    Initial commit

---
+++ src/utils/writeContacts.js
@@ -0,0 +1,7 @@
+import * as fs from 'node:fs/promises';
+
+import { PATH_DB } from '../../constants/constants.js';
+
+export const writeContacts = async (contacts) => {
+  await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+};
