cddfb6b9e601fca77d02947d06a59cc23c7f21cf
Hotsyn <85835454+SandraHotsyn@users.noreply.github.com>
2024-12-07 00:10:47 +0200

    fix path

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,5 +1,5 @@
 import * as fs from 'node:fs/promises';
-import { PATH_DB } from '../../constants/constants.js';
+import { PATH_DB } from '../constants/constants.js';
 
 export const readContacts = async () => {
   const data = await fs.readFile(PATH_DB, 'utf-8');
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,6 +1,6 @@
 import * as fs from 'node:fs/promises';
 
-import { PATH_DB } from '../../constants/constants.js';
+import { PATH_DB } from '../constants/constants.js';
 
 export const writeContacts = async (contacts) => {
   await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
